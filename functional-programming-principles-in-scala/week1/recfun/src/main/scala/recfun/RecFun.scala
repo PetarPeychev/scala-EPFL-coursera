28ba6cc371a3192c97c9554b4464bf3151bf5d86
Petar Peychev <petarpeychev98@gmail.com>
2022-05-05 01:16:08 +0100

    week 1

--- functional-programming-principles-in-scala/week1/recfun/src/main/scala/recfun/RecFun.scala
+++ functional-programming-principles-in-scala/week1/recfun/src/main/scala/recfun/RecFun.scala
@@ -5,23 +5,36 @@ object RecFun extends RecFunInterface:
   def main(args: Array[String]): Unit =
     println("Pascal's Triangle")
     for row <- 0 to 10 do
-      for col <- 0 to row do
-        print(s"${pascal(col, row)} ")
+      for col <- 0 to row do print(s"${pascal(col, row)} ")
       println()
 
-  /**
-   * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int = 
-    if (c == 0 || r == 0 || r == c) 1
-    else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  
-  /**
-   * Exercise 2
-   */
-  def balance(chars: List[Char]): Boolean = ???
+  /** Exercise 1
+    */
+  def pascal(col: Int, row: Int): Int =
+    if (col == 0 || row == 0 || row == col) 1
+    else pascal(col - 1, row - 1) + pascal(col, row - 1)
 
-  /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  /** Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean =
+
+    def balanceRec(count: Int, chars: List[Char]): Boolean =
+      if (chars.isEmpty) count == 0
+      else if (count < 0) false
+      else if (chars.head == '(') balanceRec(count + 1, chars.tail)
+      else if (chars.head == ')') balanceRec(count - 1, chars.tail)
+      else balanceRec(count, chars.tail)
+
+    balanceRec(0, chars)
+
+  /** Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int =
+
+    def countChangeRec(currentSum: Int, coins: List[Int]): Int =
+      if (currentSum == money) 1
+      else if (currentSum > money || coins.isEmpty) 0
+      else countChangeRec(currentSum + coins.head, coins) + countChangeRec(currentSum, coins.tail)
+
+    if (money == 0) 0
+    else countChangeRec(0, coins)
--- functional-programming-principles-in-scala/week1/recfun/src/main/scala/recfun/test.worksheet.sc
+++ functional-programming-principles-in-scala/week1/recfun/src/main/scala/recfun/test.worksheet.sc
@@ -1,3 +1,13 @@
 import recfun.RecFun.*
 
 pascal(1, 1)
+
+balance(("(if (zero? x) max (/ 1 x))").toList)
+
+balance(("I told him (that it's not (yet) done). (But he wasn't listening)").toList)
+
+balance(("())(").toList)
+
+balance((":-)").toList)
+
+countChange(6, List(3,2,1))
